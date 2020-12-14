b13f3f4dca585033b543ac66915aaf84e58f5d8c
Juliano Lima <juliano.lima@nubank.com.br>
2020-12-13 23:10:53 -0300

    Rename some symbols

--- src/advent_of_code/day_1.clj
+++ src/advent_of_code/day_1.clj
@@ -4,14 +4,14 @@
 (defn find-the-two-entries-that-results-in-2020
   [filename]
   (let [file-content (map #(Integer/parseInt %) (data-reader/read-input-file! filename))]
-    (loop [elements  file-content
-           tentative (first elements)]
-      (let [second-portion    (first (filter #(= (+ tentative %) 2020) elements))
-            match-condition?  (boolean second-portion)]
-        (if match-condition?
-          (* tentative second-portion)
-          (do (let [filtered-elements (remove #{tentative} elements)
-                    next-tentative (first filtered-elements)]
-               (recur filtered-elements next-tentative))))))))
+    (loop [elements    file-content
+           first-entry (first elements)]
+      (let [second-entry      (first (filter #(= (+ first-entry %) 2020) elements))
+            has-second-entry? (boolean second-entry)]
+        (if has-second-entry?
+          (* first-entry second-entry)
+          (do (let [filtered-elements (remove #{first-entry} elements)
+                    next-tentative    (first filtered-elements)]
+                (recur filtered-elements next-tentative))))))))
 
 (time (find-the-two-entries-that-results-in-2020 "resources/day_1.input"))
